40952b0d123544d550660caacf37ab091fe12f82
Paolo Speziali <speziali99@gmail.com>
2021-06-16 15:11:20 +0200

    second commit

--- markovBarze.py
+++ markovBarze.py
@@ -7,10 +7,13 @@ model_json = json.load(open("data.json",))
 text_model = markovify.Text.from_json(model_json)
 
 barze = text_model.make_sentence();
+
 if barze:
     print(barze);
 else:
-    print("cia");
+    while barze is None:
+        barze = text_model.make_sentence();
+    print(barze);
 
 
 sys.stdout.flush()
